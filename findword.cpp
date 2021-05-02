e3e26d6b41278f40bbc08a764909f2fa4a2d4ae9
yogiandaru98 <83377619+yogiandaru98@users.noreply.github.com>
2021-05-02 17:15:52 +0700

    Update and rename MENCARI_KATA.cpp to findword.cpp
    penulisan nama anggota

--- findword.cpp
+++ findword.cpp
@@ -1,3 +1,7 @@
+//ANGGOTA : 1. Yogi Andaru (2017051062)
+        //  2. Fitriah Hardianti (2057051001)
+       //   3. Muhammad Fakhri (2017051076)
+// kelas : B
 #include <iostream>
 #include <cstring>
 
