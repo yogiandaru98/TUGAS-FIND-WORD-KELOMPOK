a63fa215b1cb38fc5545a2c659ff52676226fac9
yogiandaru98 <83377619+yogiandaru98@users.noreply.github.com>
2021-05-02 22:44:50 +0700

    FINAL
    PERAPIHAN DAN PENAMBAHAN FUNGSI

--- findword.cpp
+++ findword.cpp
@@ -2,13 +2,16 @@
         //  2. Fitriah Hardianti (2057051001)
        //   3. Muhammad Fakhri (2017051076)
 // kelas : B
+//github: https://github.com/yogiandaru98/TUGAS-FIND-WORD-KELOMPOK/tree/main
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
 const int cols = 16, rows = 15;
 
- char words[rows][cols] = {"tgbwwinterwsesn",
+ char words[rows][cols] = {
+ 				"tgbwwinterwsesn",
                                 "aaunttmmhfoodnb",
                                 "jlwcqldzmpmvdmr",
                                 "asagmquwvvbsohi",
@@ -23,34 +26,17 @@ const int cols = 16, rows = 15;
                                 "mosgzczetdbooto",
                                 "pdcrzmsngrdnrpz",
                                 "ohnkzwaterjgtra"};
-
 char *getWordVertical(int);
 char *reverse(char *);
 bool searchVertical(char *);
 bool searchHorizontal(char *);
 
-
-int main()
-{
-    char word[16];
-    int n;
-    cin>>n;
-    for (int i=0;i<n;i++){
-        cin.getline(word,16,'\n');
-        if (searchVertical(word) || searchHorizontal(word))
-            cout << "Ada\n";
-        else 
-            cout << "Tidak Ada\n";
-    }
-    return 0;
-}
-
 char *getWordVertical(int idx){
 	char *CheckVertical = (char*)malloc(16 * sizeof(char));
 	for(int i=0; i<rows; i++)
 		CheckVertical[i] = words[i][idx];
-	return CheckVertical;
-        }
+	return CheckVertical;}
+//fungsi reversal
 char *reverse(char *charstring){
 	char *check1, *check2;
       if (! charstring || ! *charstring)
@@ -61,3 +47,37 @@ char *reverse(char *charstring){
         *check2 ^= *check1;
         *check1 ^= *check2;}
       return charstring;}
+//fungsi pencari kata vertikal
+bool searchVertical(char *carikan){
+	for(int i=0; i<=15; i++)
+		if(strstr(getWordVertical(i), carikan) != NULL)
+			return true;
+	for(int i=0; i<=15; i++)
+		if(strstr(reverse(getWordVertical(i)), carikan) != NULL)
+			return true;
+	return false;}
+//fungsi pencari kata horizontal
+bool searchHorizontal(char *carikan){
+	for(int i=0; i<=15; i++)
+		if(strstr(words[i], carikan) != NULL)
+			return true;
+	for(int i=0; i<=15; i++){
+		if(strstr(reverse(words[i]), carikan) != NULL)
+			return true;}
+	return false;}
+//fungsi utama program
+int main(){
+    char word[16];
+    int n;
+    cin>>n;
+    cin.ignore();
+	for (int i=0;i<n;i++){
+        cin.getline(word,16,'\n');
+        if (searchVertical(word) || searchHorizontal(word))
+            cout << "Ada\n";
+        else 
+            cout << "Tidak Ada\n";
+    }
+    
+    return 0;
+}
